697b4b060b541e74c49197d96ed4aacbc53bc5f6
Rehan Ahmad <rehanmakhdoomi93@gmail.com>
2022-11-11 11:15:14 +0530

    express.js

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+  { name: "rehan ahmad", email: "rehan@test.com" },
+  { name: "ahsan ahmad", email: "ahsan@test.com" },
+  { name: "farhan ahmad", email: "farhan@test.com" },
+];
+module.exports = data;
--- index.js
+++ index.js
@@ -1,12 +1,115 @@
+// express .js
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const publicPath = path.join(__dirname, "public");
+
+app.use(express.static(publicPath));
+app.listen(5000);
+// app.get("/", (req, res) => {
+//   res.send("Welcome, this is Home Page");
+// });
+// app.get("/about", (req, res) => {
+//   res.send("Welcome, this is About Page");
+// });
+// app.listen(5000);
+// promise
+
+// let a = 10;
+// let b = 0;
+
+// let waitingData = new Promise((resolve, reject) => {
+//   resolve(30);
+//   setTimeout(() => {
+
+//   }, 2000);
+// });
+// waitingData.then((data) => {
+//   b = data;
+//   console.log(a + b);
+// });
+// crud with file system
+
+// const fs = require("fs");
+// const path = require("path");
+// const dirPath = path.join(__dirname, "crud");
+// const filePath = `${dirPath}/apple.txt`;
+// fs.writeFileSync(filePath, "this is simple file");
+// fs.readFile(filePath, "utf8", (err, item) => {
+//   console.log(item);
+// });
+
+// update file
+
+// fs.appendFile(filePath, "file name is apple.txt", (err) => {
+//   if (!err) console.log("file is update");
+// });
+
+// rename file
+
+// fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+//   if (!err) console.log("file is update");
+// });
+
+// delete file
+
+// fs.unlinkSync(`${dirPath}/fruit.txt`);
+
+//create file with commondline
+// fs = file system
+// const fs = require("fs");
+
+// const fs = require("fs");
+// fs.writeFileSync("apple.txt", "this is a apple file");
+
+// get path directory
+// const fs = require("fs");
+// const path = require("path");
+// const dirPath = path.join(__dirname, "files");
+// for (let i = 0; i < 5; i++) {
+// fs.writeFileSync(dirPath + "/hello" + i + ".txt", "a simple tet file");
+// fs.writeFileSync(dirPath + `/hello${i}.txt`, "a simple tet file");
+// }
+// fs.readdir(dirPath, (err, files) => {
+//   files.forEach((item) => {
+//     console.log("file name is :", item);
+//   });
+// });
+// input for commandline
+
+// console.log(process.argv);
+
+// create server
+// const data = require("./data");
+// const http = require("http");
+// http
+//   .createServer((req, resp) => {
+//     resp.writeHead(200, { "Content-Type": "applicationjson" });
+//     resp.write(JSON.stringify(data));
+//     resp.end();
+//   })
+//   .listen(5000);
+
+// console.log("Try nodemonno");
+// console.log("Try nodemonno");
+// console.log(100 + 20);
+
+// color changes
+// const colors = require("colors");
+
+// console.log("pakage.json".red);
+
 // createserver
 
-const http = require("http");
-http
-  .createServer((req, resp) => {
-    resp.write("<h1>hello this is azam</h1>");
-    resp.end();
-  })
-  .listen(4500);
+// const http = require("http");
+// http
+//   .createServer((req, resp) => {
+//     resp.write("<h1>hello this is azam</h1>");
+//     resp.end();
+//   })
+//   .listen(4500);
 
 // directry name
 
